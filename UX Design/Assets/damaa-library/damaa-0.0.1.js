d870c32503ac4f95162d9e4a87c22257ec141dd1
ezeveritta <37745672+ezeveritta@users.noreply.github.com>
2018-10-29 17:44:06 -0300

    Settings workflow, project architecture. created next files:   -damaa.css (initial version: 0.0.1 CSS)   -damaa.js  (initial version: 0.0.1 JS) created Template for quick new design:   -Template     -assets       -damaa.svg     -scripts       -main.js     -styles       -master.css     -index.html
    date(2018/10/29 - 17:44)

---
+++ UX Design/Assets/damaa-library/damaa-0.0.1.js
@@ -0,0 +1,13 @@
+/*
+  * File: damaa-0.0.1.js
+  * Autor:       Ezequiel E. Vera
+  * Date:        2018/10/29 - 19:01
+  * Description: Own javasript library for personal proyects
+  * version:     0.0.1
+*/
+
+/*
+  * Quick search:
+  *  1. functions (sc: 'gotoFunctions')
+  *  2. intervals (sc: 'gotoIntervals')
+*/
---
+++ UX Design/Designs/Template/scripts/main.js
@@ -0,0 +1,18 @@
+/*
+  * File: main.js
+  * Autor:       Ezequiel E. Vera
+  * Date:        2018/10/29 - 19:31
+  * Description: describe your code here ;)
+  * version:     0.0.1
+*/
+
+/*
+  * Quick search:
+  *  1. functions, events, etc..
+*/
+
+$(function () {
+
+  // JQuery Available
+
+});
